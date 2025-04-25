203be7d06ba2acc9588b7ab39d835c7c205a106e
SaiMai <commandchannel@gmail.com>
2025-04-25 12:25:38 +0700

    fix test case

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -3,13 +3,17 @@ import * as assert from 'assert';
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
 
 suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+	test('Command: Hello World', async () => {
+		// Attempt to execute the command
+		await vscode.commands.executeCommand('fivemprojectcreate.helloWorld');
+		
+		// Since the command shows an information message, we can't directly test the result
+		// But we can at least verify the command exists
+		const commands = await vscode.commands.getCommands();
+		assert.ok(commands.includes('fivemprojectcreate.helloWorld'));
 	});
 });
