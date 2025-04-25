1efd3e11b044aed81c1cd852897a1f6b8bea2e59
SaiMai <commandchannel@gmail.com>
2025-04-25 12:50:11 +0700

    Implement project creation command with user input for project name, GUI, SQL options, and project path

--- src/extension.ts
+++ src/extension.ts
@@ -6,20 +6,105 @@ import * as vscode from 'vscode';
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "fivemprojectcreate" is now active!');
+	const userSelected = {
+		have_gui: false,
+		have_sql: false,
+		project_name: '',
+		project_path: '',
+	};
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('fivemprojectcreate.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from FivemProjectCreate!');
+	const command = vscode.commands.registerCommand('fivemprojectcreate.createProject', () => {
+		vscode.window.showInputBox({
+			title: 'Enter Project Name',
+			placeHolder: 'Please enter the project name you want to create',
+		}).then(projectName => {
+			if (!projectName) {
+				vscode.window.showErrorMessage('Project name is required');
+				return;
+			}
+
+			userSelected.project_name = projectName;
+
+			const options = {
+				title: 'Select Project Have a GUI or Not',
+				placeHolder: 'Please select the project details you want to create',
+				items: [
+					{
+						label: 'Have a GUI',
+						value: 'have_gui',
+						description: 'Create a FiveM project with a GUI'
+					},
+					{
+						label: 'No GUI', 
+						value: 'no_gui',
+						description: 'Create a FiveM project without not a GUI'
+					}
+				]
+			};
+
+			vscode.window.showQuickPick(options.items, {
+				title: options.title,
+				placeHolder: options.placeHolder
+			}).then(selection => {
+				if (!selection) {
+					userSelected.have_gui = false;
+				} else if (selection.value === 'have_gui') {
+					userSelected.have_gui = true;
+				} else {
+					userSelected.have_gui = false;
+				}
+
+				const options = {
+					title: 'Select Project Have a SQL or Not',
+					placeHolder: 'Please select the project details you want to create',
+					items: [
+						{
+							label: 'Have a SQL',
+							value: 'have_sql',
+							description: 'Create a FiveM project with a SQL'
+						},
+						{
+							label: 'No SQL',
+							value: 'no_sql',
+							description: 'Create a FiveM project without not a SQL'
+						}
+					]
+				};
+
+				vscode.window.showQuickPick(options.items, {
+					title: options.title,
+					placeHolder: options.placeHolder
+				}).then(selection => {
+					if (!selection) {
+						userSelected.have_sql = false;
+					} else if (selection.value === 'have_sql') {
+						userSelected.have_sql = true;
+					} else {
+						userSelected.have_sql = false;
+					}
+
+					vscode.window.showInputBox({
+						title: 'Enter Project Path',
+						placeHolder: 'Please enter the project path you want to create',
+					}).then(projectPath => {
+						if (!projectPath) {
+							userSelected.project_path = '.';
+						} else {
+							userSelected.project_path = projectPath;
+						}
+
+						createProject(userSelected);
+					});
+				});
+			});
+		});
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(command);
+}
+
+function createProject(userSelected: any) {
+	
 }
 
 // This method is called when your extension is deactivated
